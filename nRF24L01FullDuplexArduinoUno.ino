2ab05decae071d56df79f9ef09d8364514e1e3ea
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-10-26 17:45:04 +0100

    Update nRF24L01FullDuplexArduinoUno.ino

--- nRF24L01FullDuplexArduinoUno.ino
+++ nRF24L01FullDuplexArduinoUno.ino
@@ -1,23 +1,18 @@
 // ----------------------------------------------------------------------------------------------------------------------------------
 // Full Duplex Arduino UNO Code for Transmit and Receive CUSTOM Messsage.
-// Created By Dominik Hulin
-//         : LINKS :
-// GitHub  : github.com/Fattcat
-// YouTube : https://www.youtube.com/channel/UCKfToKJFq-uvI8svPX0WzYQ
+
 
 //         : Components :
 // nRF24L01 
 // Cost : About 2 €  
-// AliExpress Link : https://www.aliexpress.com/item/32517849393.html?spm=a2g0o.order_list.order_list_main.5.53341802zR7CBu
-//
+
 // Arduino UNO
 // Cost : About 3 €
-// AliExpress Link : https://www.aliexpress.com/item/32932088536.html?spm=a2g0o.order_list.order_list_main.15.272c1802A1wYyH
+
 //
 // Some Wires for Connect it
 // Cost : About 1 €
-// AliExpress Link : https://www.aliexpress.com/item/1005005945668553.html?spm=a2g0o.productlist.main.5.3ee96589bktPwT&algo_pvid=10843895-becd-41e7-bb9e-6ba514305112&algo_exp_id=10843895-becd-41e7-bb9e-6ba514305112-2&pdp_npi=4%40dis%21EUR%215.02%211.74%21%21%2137.99%21%21%40211b600e16979140499187979e18b2%2112000034970736149%21sea%21SK%214031371332%21&curPageLogUid=jRvt0BDum3lC
-// ----------------------------------------------------------------------------------------------------------------------------------
+ ----------------------------------------------------------------------------------------------------------------------------------
 
 
 #include <SPI.h>
